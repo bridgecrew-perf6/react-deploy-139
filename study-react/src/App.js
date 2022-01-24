0c05e9d3706cdd62c5657165c4fd45dee54ae9e5
SangWook-H <hansanguk950112@gmail.com>
2022-01-24 17:17:03 +0900

    liefCycle practice

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-import ScrollBox from "./ScrollBox";
-const App = () => {
-  return (
-    <div>
-      <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-      <button onClick={() => this.scrollBox.scrollToBottom()} />
-    </div>
-  );
-};
+import React, { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
+}
 
 export default App;
